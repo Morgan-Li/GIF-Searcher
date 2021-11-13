0207c38e7d9a9f9cdce9dfab44013922a1e3622d
Morgan Li <Morganli135@Gmail.com>
2021-11-12 20:39:26 -0700

    Added ability to refresh without initial user input

--- script.js
+++ script.js
@@ -45,4 +45,18 @@ refresh.addEventListener("click", () => {
     .catch(e => {
       console.log(e)
     })
+
+    if(!userInput) {
+        fetch('https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=cat', {mode: 'cors'})
+        .then(function(response) {
+          return response.json()
+        })
+        .then(function(response) {
+          img.src = response.data.images.original.url
+          document.getElementById("GIF-URL").innerHTML = 'GIF URL: ' + response.data.images.original.url
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
   })

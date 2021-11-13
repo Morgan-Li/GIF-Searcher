7d618493f4137ed38c5afbb5bfd91277bfd5cfbe
Morgan Li <Morganli135@Gmail.com>
2021-11-12 19:33:15 -0700

    Added a refresh button

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const img = document.querySelector('img')
 const submit = document.getElementById('submitBtn');
+const refresh = document.getElementById('refreshBtn');
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=cat', {mode: 'cors'})
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(response) {
+    img.src = response.data.images.original.url
+  })
+  .catch(e => {
+    console.log(e)
+  })
+
+
 submit.addEventListener("click", () => {
   const userInput = form.search.value;
   console.log(userInput);
@@ -15,13 +29,17 @@ submit.addEventListener("click", () => {
   })
 })
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=cat', {mode: 'cors'})
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url
-  })
-  .catch(e => {
-    console.log(e)
+refresh.addEventListener("click", () => {
+    const userInput = form.search.value;
+    console.log(userInput);
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=${userInput}`, {mode: 'cors'})
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url
+    })
+    .catch(e => {
+      console.log(e)
+    })
   })
